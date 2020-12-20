60c1d07b51ac447c70519e6bd2338f5b9545b8cf
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-12-20 11:54:03 +0200

    viewer-electron > dev: renderer > logic/loaders/eimg

---
+++ packages/viewer-electron/source/renderer/modules/services/logic/loaders/eimg/index.ts
@@ -0,0 +1,26 @@
+// #region imports
+    // #region libraries
+    import {
+        Reader,
+    } from '@plurid/enhanced-image-io`;
+    // #endregion libraries
+// #endregion imports
+
+
+
+// #region module
+const eimgLoad = (
+    filepath: string,
+) =>  {
+    const reader = new Reader(filepath);
+    const data = await reader.read();
+
+    return data;
+}
+// #endregion module
+
+
+
+// #region exports
+export default eimgLoad;
+// #endregion exports
