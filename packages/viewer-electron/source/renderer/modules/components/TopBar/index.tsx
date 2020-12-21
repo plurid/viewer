e5857e484590ded28cec6b8ef52ba90d78b14b22
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-12-21 07:39:38 +0200

    viewer-electron > dev: components/TopBar

--- packages/viewer-electron/source/renderer/modules/components/TopBar/index.tsx
+++ packages/viewer-electron/source/renderer/modules/components/TopBar/index.tsx
@@ -1,33 +1,153 @@
-import React, {
-    useState,
-} from 'react';
+// #region imports
+    // #region libraries
+    import React, {
+        useRef,
+        useState,
+        useEffect,
+    } from 'react';
 
-import {
-    plurid,
-} from '@plurid/plurid-themes';
+    import { AnyAction } from 'redux';
+    import { connect } from 'react-redux';
+    import { ThunkDispatch } from 'redux-thunk';
 
-import {
-    StyledTopBar,
-} from './styled';
+    import {
+        Theme,
+    } from '@plurid/plurid-themes';
+    // #endregion libraries
+
+
+    // #region external
+    import { AppState } from '~renderer-services/state/store';
+    import StateContext from '~renderer-services/state/context';
+    import selectors from '~renderer-services/state/selectors';
+    // import actions from '~renderer-services/state/actions';
+    // #endregion external
+
+
+    // #region internal
+    import {
+        StyledTopBar,
+    } from './styled';
+    // #endregion internal
+// #endregion imports
 
 
 
-const theme = plurid;
+// #region module
+export interface TopBarOwnProperties {
+}
 
-const TopBar: React.FC<any> = () => {
-    const [mouseOver, setMouseOver] = useState(false);
+export interface TopBarStateProperties {
+    stateGeneralTheme: Theme;
+    stateInteractionTheme: Theme;
+}
 
+export interface TopBarDispatchProperties {
+}
+
+export type TopBarProperties = TopBarOwnProperties
+    & TopBarStateProperties
+    & TopBarDispatchProperties;
+
+const TopBar: React.FC<TopBarProperties> = (
+    properties,
+) => {
+    // #region properties
+    const {
+        // #region state
+        stateGeneralTheme,
+        // stateInteractionTheme,
+        // #endregion state
+    } = properties;
+    // #endregion properties
+
+
+    // #region references
+    const hoverOutTimeout = useRef<NodeJS.Timeout | null>(null);
+    // #endregion references
+
+
+    // #region state
+    const [
+        mouseOver,
+        setMouseOver,
+    ] = useState(false);
+
+    const [
+        show,
+        setShow,
+    ] = useState(false);
+    // #endregion state
+
+
+    // #region effects
+    useEffect(() => {
+        if (mouseOver) {
+            setShow(true);
+            return;
+        }
+
+        if (!mouseOver) {
+            hoverOutTimeout.current = setTimeout(
+                () => {
+                    setShow(false);
+                },
+                500,
+            );
+        }
+
+        return () => {
+            if (hoverOutTimeout.current) {
+                clearTimeout(hoverOutTimeout.current);
+            }
+        }
+    }, [
+        mouseOver,
+    ]);
+    // #endregion effects
+
+
+    // #region render
     return (
         <StyledTopBar
             onMouseEnter={() => setMouseOver(true)}
             onMouseLeave={() => setMouseOver(false)}
             onMouseMove={() => !mouseOver ? setMouseOver(true) : undefined}
-            mouseOver={mouseOver}
-            theme={theme}
+            show={show}
+            theme={stateGeneralTheme}
         >
         </StyledTopBar>
     );
+    // #endregion render
 }
 
 
-export default TopBar;
+const mapStateToProperties = (
+    state: AppState,
+): TopBarStateProperties => ({
+    stateGeneralTheme: selectors.themes.getGeneralTheme(state),
+    stateInteractionTheme: selectors.themes.getInteractionTheme(state),
+});
+
+
+const mapDispatchToProperties = (
+    dispatch: ThunkDispatch<{}, {}, AnyAction>,
+): TopBarDispatchProperties => ({
+});
+
+
+const ConnectedTopBar = connect(
+    mapStateToProperties,
+    mapDispatchToProperties,
+    null,
+    {
+        context: StateContext,
+    },
+)(TopBar);
+// #endregion module
+
+
+
+// #region exports
+export default ConnectedTopBar;
+// #endregion exports
