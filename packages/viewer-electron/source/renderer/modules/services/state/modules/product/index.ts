41e602c51eb439621a54ba11879e1a292481489c
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-08-18 23:06:03 +0300

    viewer-electron > dev: renderer | services/state

--- packages/viewer-electron/source/renderer/modules/services/state/modules/product/index.ts
+++ packages/viewer-electron/source/renderer/modules/services/state/modules/product/index.ts
@@ -1,23 +1,292 @@
 // #region imports
+    // #region libraries
+    import {
+        createSlice,
+        PayloadAction,
+    } from '@reduxjs/toolkit';
+
+    import {
+        uuid,
+    } from '@plurid/plurid-functions';
+    // #endregion libraries
+
+
+    // #region external
+    import {
+        FileStream,
+        Plane,
+    } from '~renderer-data/interfaces';
+
+    import type {
+        AppState,
+    } from '~renderer-services/state/store';
+    // #endregion external
+
+
     // #region internal
-    import actions from './actions';
-    import initialState from './initial';
-    import reducer from './reducer';
-    import resolvers from './resolvers';
-    import selectors from './selectors';
     import * as Types from './types';
+    import _initialState from './initial';
+    import _selectors from './selectors';
     // #endregion internal
 // #endregion imports
 
 
 
-// #region exports
-export {
-    actions,
-    initialState,
-    reducer,
-    resolvers,
-    selectors,
-    Types,
+// #region module
+export type ProductState = Types.State;
+
+
+const initialState: ProductState = {
+    ..._initialState,
 };
+
+
+export interface SetFieldPayload {
+    field: keyof ProductState;
+    data: any;
+}
+
+export interface AddPlanePayload {
+    spaceID: string;
+    data: Plane;
+}
+
+export interface UpdatePlanePayload {
+    spaceID: string;
+    planeID: string;
+    data: Partial<any>;
+}
+
+export interface RemovePlanePayload {
+    spaceID: string;
+    planeID: string;
+}
+
+export interface AddStreamPayload {
+    id: string;
+    filepath: string;
+    url: string;
+}
+
+
+export const name = 'product' as const;
+
+
+export const product = createSlice({
+    name,
+    initialState,
+    reducers: {
+        setProduct: (
+            state,
+            action: PayloadAction<ProductState>,
+        ) => {
+            return {
+                ...state,
+                ...action.payload,
+            };
+        },
+        unsetProduct: (
+            state,
+        ) => {
+            state = {
+                ...initialState,
+            };
+        },
+        setLanguage: (
+            state,
+            action: PayloadAction<string>,
+        ) => {
+            state.language = action.payload;
+        },
+        setField: (
+            state,
+            action: PayloadAction<SetFieldPayload>,
+        ) => {
+            const {
+                field,
+                data,
+            } = action.payload;
+
+            (state as any)[field] = data;
+        },
+        addPlane: (
+            state,
+            action: PayloadAction<AddPlanePayload>,
+        ) => {
+            const {
+                spaceID,
+                data,
+            } = action.payload;
+
+            state.spaces = state.spaces.map(space => {
+                if (space.id === spaceID) {
+                    return {
+                        ...space,
+                        planes: [
+                            ...space.planes,
+                            {
+                                ...data,
+                            },
+                        ],
+                    };
+                }
+
+                return {
+                    ...space,
+                };
+            });
+        },
+        updatePlane: (
+            state,
+            action: PayloadAction<UpdatePlanePayload>,
+        ) => {
+            const {
+                spaceID,
+                planeID,
+                data,
+            } = action.payload;
+
+            const space = state.spaces.find(space => space.id === spaceID);
+            if (!space) {
+                return;
+            }
+
+            const plane = space.planes.find(plane => plane.id === planeID);
+            if (!plane) {
+                return;
+            }
+
+            for (const [key, value] of Object.entries(data)) {
+                plane.data[key] = value;
+            }
+
+            const planes = space.planes.map(spacePlane => {
+                if (spacePlane.id === planeID) {
+                    return plane;
+                }
+
+                return spacePlane;
+            });
+
+            space.planes = [
+                ...planes,
+            ];
+
+            state.spaces = state.spaces.map(stateSpace => {
+                if (stateSpace.id === spaceID) {
+                    return space;
+                }
+
+                return stateSpace;
+            });
+        },
+        removePlane: (
+            state,
+            action: PayloadAction<RemovePlanePayload>,
+        ) => {
+            const {
+                spaceID,
+                planeID,
+            } = action.payload;
+
+            state.spaces = state.spaces.map(space => {
+                if (spaceID === space.id) {
+                    const planes = space.planes.filter(plane => plane.id !== planeID);
+
+                    return {
+                        ...space,
+                        planes,
+                    };
+                }
+
+                return {
+                    ...space,
+                };
+            });
+        },
+        addSpace: (
+            state,
+        ) => {
+            const spaces = [
+                ...state.spaces,
+                {
+                    id: uuid.generate(),
+                    name: 'new space',
+                    order: state.spaces.length,
+                    planes: [],
+                },
+            ];
+
+            state.spaces = spaces;
+        },
+        removeSpace: (
+            state,
+            action: PayloadAction<string>,
+        ) => {
+            const spaces = state.spaces.filter(space => space.id !== action.payload);
+
+            if (spaces.length === 0) {
+                spaces.push(
+                    {
+                        id: uuid.generate(),
+                        name: 'new space',
+                        order: state.spaces.length,
+                        planes: [],
+                    },
+                );
+            }
+
+            state.spaces = spaces;
+        },
+        addStream: (
+            state,
+            action: PayloadAction<AddStreamPayload>,
+        ) => {
+            const {
+                payload,
+            } = action;
+
+            const {
+                id,
+                filepath,
+                url,
+            } = payload;
+
+
+            const fileStream: FileStream = {
+                id,
+                filepath,
+                url,
+                localport: 9090,
+                local: true,
+                global: false,
+            };
+
+
+            const newFilesStreams = {
+                ...state.filesStreams,
+            };
+            newFilesStreams[id] = {
+                ...fileStream,
+            };
+            state.filesStreams = newFilesStreams;
+        },
+    },
+});
+// #endregion module
+
+
+
+// #region exports
+export const actions = product.actions;
+
+
+// export const getProduct = (state: AppState) => state.product;
+
+export const selectors = {
+    ..._selectors,
+};
+
+
+export const reducer = product.reducer;
 // #endregion exports
--- packages/viewer-electron/source/renderer/modules/services/state/modules/product/types/index.ts
+++ packages/viewer-electron/source/renderer/modules/services/state/modules/product/types/index.ts
@@ -2,7 +2,6 @@
     // #region external
     import {
         Space,
-        Plane,
         RecordFilesStreams,
     } from '~renderer-data/interfaces';
     // #endregion external
@@ -11,97 +10,6 @@
 
 
 // #region module
-export const SET_PRODUCT = 'SET_PRODUCT';
-export interface SetProductAction {
-    type: typeof SET_PRODUCT;
-    payload: any;
-}
-
-
-export const UNSET_PRODUCT = 'UNSET_PRODUCT';
-export interface UnsetProductAction {
-    type: typeof UNSET_PRODUCT;
-}
-
-
-export const SET_LANGUAGE = 'SET_LANGUAGE';
-export interface SetLanguageAction {
-    type: typeof SET_LANGUAGE;
-    payload: string;
-}
-
-
-export const SET_FIELD = 'SET_FIELD';
-export interface SetFieldPayload {
-    field: keyof State;
-    data: any;
-}
-export interface SetFieldAction {
-    type: typeof SET_FIELD;
-    payload: SetFieldPayload;
-}
-
-
-export const ADD_PLANE = 'ADD_PLANE';
-export interface AddPlanePayload {
-    spaceID: string;
-    data: Plane;
-}
-export interface AddPlaneAction {
-    type: typeof ADD_PLANE;
-    payload: AddPlanePayload;
-}
-
-
-export const UPDATE_PLANE = 'UPDATE_PLANE';
-export interface UpdatePlanePayload {
-    spaceID: string;
-    planeID: string;
-    data: Partial<any>;
-}
-export interface UpdatePlaneAction {
-    type: typeof UPDATE_PLANE;
-    payload: UpdatePlanePayload;
-}
-
-
-export const REMOVE_PLANE = 'REMOVE_PLANE';
-export interface RemovePlanePayload {
-    spaceID: string;
-    planeID: string;
-}
-export interface RemovePlaneAction {
-    type: typeof REMOVE_PLANE;
-    payload: RemovePlanePayload;
-}
-
-
-export const ADD_SPACE = 'ADD_SPACE';
-export interface AddSpaceAction {
-    type: typeof ADD_SPACE;
-}
-
-
-export const REMOVE_SPACE = 'REMOVE_SPACE';
-export interface RemoveSpaceAction {
-    type: typeof REMOVE_SPACE;
-    payload: string;
-}
-
-
-export const ADD_STREAM = 'ADD_STREAM';
-export interface AddStreamPayload {
-    id: string;
-    filepath: string;
-    url: string;
-}
-export interface AddStreamAction {
-    type: typeof ADD_STREAM;
-    payload: AddStreamPayload;
-}
-
-
-
 export interface State {
     ui: {
         toolbars: any;
@@ -124,17 +32,4 @@ export interface State {
     filesDefaultPluridLinkNavigation: boolean;
     filesStreams: RecordFilesStreams;
 }
-
-
-export type Actions =
-    | SetProductAction
-    | UnsetProductAction
-    | SetLanguageAction
-    | SetFieldAction
-    | AddPlaneAction
-    | UpdatePlaneAction
-    | RemovePlaneAction
-    | AddSpaceAction
-    | RemoveSpaceAction
-    | AddStreamAction;
 // #endregion module
