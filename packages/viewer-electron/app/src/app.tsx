83f726a7a92fad7e536a3534fbe3ceee7c9ea64e
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2025-01-31 20:17:24 +0200

    viewer-electron > setup: app | react

---
+++ packages/viewer-electron/app/src/app.tsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client';
+
+
+
+const root = createRoot(document.body);
+root.render(<h2
+    style={{
+        color: 'white',
+    }}
+>
+    renderer
+</h2>);
